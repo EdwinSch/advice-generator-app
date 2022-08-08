ae0167bbd5364970ac7a116490f29380fb8eda03
EdwinSch <el.schutjes@avans.nl>
2022-08-08 13:14:58 +0200

    added JavaScript and updated stylig

--- getapi.js
+++ getapi.js
@@ -1,6 +1,41 @@
+// Selectors
+const slipNumber = document.querySelector('h2');
+const advice = document.getElementById('advice');
+const diceBtn = document.getElementById('dice-button');
+
+// On DOM load get advice
+window.addEventListener('DOMContentLoaded', (event) => {
+    getAdvice();
+});
+
+// Dice button function
+diceBtn.addEventListener('click', function () {
+    getAdvice();
+})
+
+
+// Get advice from API function && push to DOM
+async function getAdvice() {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const data = await response.json()
+    // console.log(data);
+    slipNumber.innerHTML = `advice #${data.slip.id}`
+    advice.innerHTML = `&#34;${data.slip.advice}&#34;`
+};
+
+
+
+/* ALTERNATIVE: fetch .then method
 
 fetch('https://api.adviceslip.com/advice')
-    .then(response => response.json())
-    .then(data => console.log(data))
+    .then((response) => response.json())
+    .then((data) => {
+        slipNumber.innerHTML = data.slip.id,
+            advice.innerHTML = data.slip.advice
+    }
+    );
+*/
+
+
 
 
